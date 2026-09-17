4d1032af3a67d3f036de9d26d697b87e04bb4adb
dnmore <morettidn@gmail.com>
2026-09-17 17:19:19 +0200

    update button

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="p-4 w-full bg-sidebar rounded-xl my-2 border shadow-sm">
+      <main className="p-4 w-full bg-sidebar rounded-xl m-2 border shadow-sm">
         <SidebarTrigger />
         {children}
       </main>
