8d298ccf45b88c3096ab576d0161c6b42cbf9e88
dnmore <morettidn@gmail.com>
2026-09-17 16:57:04 +0200

    update layout

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,15 +1,16 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="p-4 w-full bg-sidebar">
-        <SidebarTrigger />
-        {children}
+      <main>
+        <div className="p-4 w-full bg-sidebar rounded-xl my-2">
+          <SidebarTrigger />
+          {children}
+        </div>
       </main>
     </SidebarProvider>
-  )
+  );
 }
